eed5731912b022280b3c1624e4a8e71c5260b9ec
Thông Trần <quangthong211101@gmail.com>
2024-02-24 09:07:04 +0700

    Add model Login in Area Admin

---
+++ BookSale.Management.UI/Areas/Admin/Controllers/AuthenticationController.cs
@@ -0,0 +1,28 @@
+﻿using BookSale.Management.UI.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookSale.Management.UI.Areas.Admin.Controllers
+{
+    //domain/Admin/Authentication/{Action}
+    [Area("Admin")]
+    public class AuthenticationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]//Default
+        public IActionResult Login()
+        {
+            var mdLogin = new LoginModel();
+            return View(mdLogin);
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel loginModel)
+        {
+            return View();
+        }
+    }
+}
---
+++ BookSale.Management.UI/Areas/Admin/Models/LoginModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BookSale.Management.UI.Areas.Admin.Models
+{
+    public class LoginModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool HasRememberMe { get; set; }
+    }
+}
