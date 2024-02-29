cd8dd729edbab3c2daad7dd3929e3f87ffdb3ea2
Thông Trần <quangthong211101@gmail.com>
2024-02-29 10:06:02 +0700

    Hiển thị preview image khi update or create

---
+++ BookSale.Management.UI/wwwroot/Admin/components/account_savedata.js
@@ -0,0 +1,22 @@
+﻿(function () {
+
+    const img = document.getElementById('img-avatar');
+
+    document.getElementById('Avatar').onchange = function () {
+        const input = document.getElementById('Avatar').files[0];
+
+        if (input) {
+            img.src = URL.createObjectURL(input);
+        }
+    }
+
+    //Xử lý khi ảnh đã bị xoá và thay bằng ảnh khác
+    img.onerror = function () {
+        onErrorImages();
+    }
+
+    function onErrorImages() {
+        img.src = "/images/image.png";
+        img.alt = "no image";
+    }
+})();
