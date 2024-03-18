d81f6533f5ac6bcf3f118909a4d8120286e8a2b6
PanPan <quangthong211101@gmail.com>
2024-03-18 15:41:26 +0700

    Add layout customer

---
+++ BookSale.Management.UI/Controllers/ShopController.cs
@@ -0,0 +1,22 @@
+﻿using BookSale.Management.Application.Abstracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookSale.Management.UI.Controllers
+{
+	public class ShopController : Controller
+	{
+		private readonly IGenreService _genreService;
+		private readonly IBookService _bookService;
+
+		public ShopController(IGenreService genreService, IBookService bookService)
+        {
+			_genreService = genreService;
+			_bookService = bookService;
+		}
+        
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
