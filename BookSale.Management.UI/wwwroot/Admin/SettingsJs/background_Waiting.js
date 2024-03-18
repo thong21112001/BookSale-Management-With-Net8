bbc4dbf8b6de340fd2b5b3c1398ef2df05b3e935
Thông Trần <quangthong211101@gmail.com>
2024-03-18 09:23:20 +0700

    Tạo background waiting khi generate code book

---
+++ BookSale.Management.UI/wwwroot/Admin/SettingsJs/background_Waiting.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    $.blockUI.defaults.message = `<div class="sk-circle">
+                                      <div class="sk-circle1 sk-child"></div>
+                                      <div class="sk-circle2 sk-child"></div>
+                                      <div class="sk-circle3 sk-child"></div>
+                                      <div class="sk-circle4 sk-child"></div>
+                                      <div class="sk-circle5 sk-child"></div>
+                                      <div class="sk-circle6 sk-child"></div>
+                                      <div class="sk-circle7 sk-child"></div>
+                                      <div class="sk-circle8 sk-child"></div>
+                                      <div class="sk-circle9 sk-child"></div>
+                                      <div class="sk-circle10 sk-child"></div>
+                                      <div class="sk-circle11 sk-child"></div>
+                                      <div class="sk-circle12 sk-child"></div>
+                                </div>`
+
+    $.blockUI.defaults.css = {
+        padding: 0,
+        margin: 0,
+        width: '30%',
+        top: '40%',
+        left: '35%',
+        textAlign: 'center',
+        color: '#000',
+        border: 'none',
+        backgroundColor: 'none',
+        cursor: 'wait'
+    }
+})();
--- BookSale.Management.UI/wwwroot/Admin/components/book_savedata.js
+++ BookSale.Management.UI/wwwroot/Admin/components/book_savedata.js
@@ -1,9 +1,23 @@
 ﻿(function () {
     $(document).on('click', '#btn-generate', function () {
+
+        $.blockUI();
+
         $.ajax({
             url: '/admin/book/generatecodebook',
             success: function (response) {
                 $('#Code').val(response);
+
+                //Dừng 3s
+                setTimeout(() => {
+                    $.unblockUI();
+                }, 3000);
+            },
+            error: function (response) {
+                //Dừng 3s
+                setTimeout(() => {
+                    $.unblockUI();
+                }, 2000);
             }
         });
     });
