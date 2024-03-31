b5f85d00f6ada912ff545e5cddc6db3a70f38469
PanPan <quangthong211101@gmail.com>
2024-03-31 21:17:37 +0700

    Xử lý không có sản phẩm trong giỏ hàng, sau khi xóa

--- BookSale.Management.UI/wwwroot/User/config_application/cart/buttonDeleteItemInCart.js
+++ BookSale.Management.UI/wwwroot/User/config_application/cart/buttonDeleteItemInCart.js
@@ -10,14 +10,29 @@
                 if (result.success) {
                     // Xóa sản phẩm khỏi giao diện
                     $(`tr[data-code="${result.codeBook}"]`).remove();
+
                     // Cập nhật tổng giá tiền của giỏ hàng
                     $('#cartTotal').text(result.cartTotal);
+
+                    // Kiểm tra nếu không còn sản phẩm nào trong giỏ hàng
+                    if (result.itemCartTotal === 0) {
+                        showToastAllPage("info", "Giỏ hàng đang trống. Trang sẽ tự động reload sau 6 giây.");
+
+                        // Đặt timeout để reload trang sau 3 giây
+                        setTimeout(function () {
+                            window.location.reload();
+                        }, 6000);
+                    }
+
                     // Cập nhật tổng item của giỏ hàng
                     $('#itemCartTotal').text(result.itemCartTotal);
+
                     //Cập nhập lại số lượng hiển thị item trên giỏ hàng
                     updateCartCount();
+
                     //Hiển thị thông báo thành công
                     showToastAllPage("success", "Xoá sản phẩm thành công !!!");
+
                 } else {
                     showToastAllPage("error", "Xoá sản phẩm thất bại...");
                 }
