2c1c450f5d368c2fd9dab7612fec8db1a59aa9a7
Thông Trần <quangthong211101@gmail.com>
2024-04-05 09:19:47 +0700

    Tạo UI cho trang profile user

---
+++ BookSale.Management.UI/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookSale.Management.UI.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
