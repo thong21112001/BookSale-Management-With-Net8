f2fe68764cd346238c84e81c8ef8b356c939458f
Thông Trần <quangthong211101@gmail.com>
2024-04-18 15:55:43 +0700

    Hoàn thành phần thanh toán

---
+++ BookSale.Management.UI/wwwroot/User/config_application/checkout/buttonCheckForm.js
@@ -0,0 +1,17 @@
+﻿const inputFields = document.querySelectorAll('#input-fields input.required-field');
+const paymentSections = document.querySelectorAll('.hidden');
+const proceedButton = document.querySelector('#showPayment');
+
+function togglePaymentSection() {
+    const allFieldsFilled = Array.from(inputFields).every(input => input.value.trim() !== '' && input.checkValidity());
+
+    paymentSections.forEach(section => {
+        if (allFieldsFilled) {
+            section.classList.remove('hidden');
+        } else {
+            section.classList.add('hidden');
+        }
+    });
+}
+
+proceedButton.addEventListener('click', togglePaymentSection);
---
+++ BookSale.Management.UI/wwwroot/User/config_application/checkout/radioChangePayment.js
@@ -0,0 +1,23 @@
+﻿// Lấy các phần tử input radio và các phần DIV tương ứng
+const cashRadio = document.querySelector('input[value="1"]');
+const paypalRadio = document.querySelector('input[value="2"]');
+const cashDiv = document.querySelector('#cashDiv');
+const paypalDiv = document.querySelector('#paypalDiv');
+
+// Ẩn phần DIV Paypal ban đầu
+paypalDiv.style.display = 'none';
+
+// Đặt sự kiện "change" cho input radio
+cashRadio.addEventListener('change', function () {
+    if (cashRadio.checked) {
+        cashDiv.style.display = 'block'; // Hiển thị phần DIV Cash
+        paypalDiv.style.display = 'none'; // Ẩn phần DIV Paypal
+    }
+});
+
+paypalRadio.addEventListener('change', function () {
+    if (paypalRadio.checked) {
+        paypalDiv.style.display = 'block'; // Hiển thị phần DIV Paypal
+        cashDiv.style.display = 'none'; // Ẩn phần DIV Cash
+    }
+});
