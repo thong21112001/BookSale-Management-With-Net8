ea8ed03a41c926f52380cb878ac55d7b794ce2f2
Thông Trần <quangthong211101@gmail.com>
2024-05-26 16:06:31 +0700

    Cài đặt lại layout và hiển thị thông tin table cho đẹp

--- BookSale.Management.UI/wwwroot/Admin/components/order_showdata.js
+++ BookSale.Management.UI/wwwroot/Admin/components/order_showdata.js
@@ -2,26 +2,41 @@
 
 (function () {
     const columns = [
-        { data: 'Id', name: 'Id', autoWidth: true },
-        { data: 'Code', name: 'Code', autoWidth: true },
         {
-            data: 'CreatedOn', name: 'CreatedOn', autoWidth: true, render: function (data) {
+            data: 'Id', name: 'Id', width: '150px', render: function (key) {
+                return `
+                    <span data-key="${key}">
+                            <a class="btn-edit btn btn-icon btn-warning btn-sm mr-2"><i class="fas fa-pencil-alt"></i></a>
+                            &nbsp
+							<a onClick=Delete('/admin/order/delete/${key}') class="btn btn-icon btn-danger btn-sm mr-2"><i class="far fa-trash-alt"></i></a>
+                            &nbsp
+                            <a class="btn-dowload btn btn-icon btn-info btn-sm mr-2"><i class="far fa-arrow-alt-circle-down"></i></a>
+                    </span>
+                `
+            }
+        },
+        { data: 'Fullname', name: 'Fullname', width: "300px" },
+        { data: 'Code', name: 'Code', width: "150px" },
+        {
+            data: 'CreatedOn', name: 'CreatedOn', width: "150px", render: function (data) {
                 return `<div class="text-left">${moment(data).format("DD/MM/YYYY")}</div>`
             }
         },
-        { data: 'PaymentMethod', name: 'PaymentMethod', width: "60px" },
         {
-            data: 'Status', name: 'Status', width: "60px", render: function (data) {
-                return `<div class="text-center">${formatStatus(data)}</div>`
-            }
-        },
-        { data: 'Fullname', name: 'Fullname', autoWidth: true },
-        {
-            data: 'TotalPrice', name: 'TotalPrice', autoWidth: true, render: function (data) {
+            data: 'TotalPrice', name: 'TotalPrice', width: "150px", render: function (data) {
                 return `<div class="text-left">${data.toLocaleString('vi-VN', {
                     style: 'currency',
                     currency: 'VND'
                 })}</div>`
+            }
+        },
+        {
+            data: 'PaymentMethod', name: 'PaymentMethod', width: "60px", render: function (data) {
+                return `<div class="text-center">${data}</div>`
+            } },
+        {
+            data: 'Status', name: 'Status', width: "60px", render: function (data) {
+                return `<div class="text-center">${formatStatus(data)}</div>`
             }
         }
     ];
